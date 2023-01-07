d3e7e5b22720f4dd44bbda0a8a9a8f5bd55ed18d
Marno (Mathinus) Nel <marthinusnel2023@u.northwestern.edu>
2023-01-07 14:49:12 -0600

    Can launch rviz with robot tf

---
+++ nuturtle_description/launch/load_one.launch.py
@@ -0,0 +1,46 @@
+"""
+Starts all the nodes to visualize a turtlebot3 in rviz
+"""
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, PathJoinSubstitution, TextSubstitution,\
+     LaunchConfiguration
+from launch.conditions import IfCondition, LaunchConfigurationEquals
+from launch_ros.substitutions import FindPackageShare, ExecutableInPackage
+from launch.actions import Shutdown
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument(name="use_jsp", default_value="true",
+                              description="true: use joint_state_publisher, false: no joint states \
+                                           published"),
+        DeclareLaunchArgument(name="use_rviz", default_value="true",
+                              description="true: start rviz, false: don't start rviz"),
+        Node(package="joint_state_publisher",
+             executable="joint_state_publisher",
+             condition= LaunchConfigurationEquals("use_jsp", "true")
+             ),
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            condition= LaunchConfigurationEquals("use_jsp", "true"),
+            parameters=[
+                {"robot_description" :
+                Command([ExecutableInPackage("xacro", "xacro"), " ",
+                         PathJoinSubstitution(
+                        [FindPackageShare("nuturtle_description"), 
+                                          "urdf/turtlebot3_burger.urdf.xacro"])])}
+                ]
+                ),
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            arguments=["-d",
+                       PathJoinSubstitution(
+                           [FindPackageShare("nuturtle_description"), "config/basic_purple.rviz"])],
+            condition=LaunchConfigurationEquals("use_rviz", "true"),
+            on_exit = Shutdown()
+            )
+        ])
