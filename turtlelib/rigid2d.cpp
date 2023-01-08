9f46b9900ecb6124f16557765c66f155ad850cdc
Marno (Mathinus) Nel <marthinusnel2023@u.northwestern.edu>
2023-01-07 18:55:50 -0600

    Started with function implementations

---
+++ turtlelib/rigid2d.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include "rigid2d.hpp" // contains forward definitions for iostream objects
+
+
+constexpr bool almost_equal(double d1, double d2, double epsilon=1.0e-12)
+{
+    return std::abs(d1 - d2) < epsilon;
+}
+
+constexpr double deg2rad(double deg)
+{
+    return (deg * PI) / 180;
+}
+
+constexpr double rad2deg(double rad)
+{
+    return (rad * 180) / PI;
+}
