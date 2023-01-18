346ed0bb36aeaf6482449a73ffd6f8ffc9c91650
Marno (Mathinus) Nel <marthinusnel2023@u.northwestern.edu>
2023-01-18 16:55:42 -0600

    Added rate param

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -5,6 +5,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/u_int64.hpp"
 
 using namespace std::chrono_literals;
 
@@ -15,25 +16,26 @@ class Nusim : public rclcpp::Node
 {
   public:
     Nusim()
-    : Node("Nusim"), count_(0)
+    : Node("Nusim"), timestep_(0)
     {
-      this->declare_parameter("rate", 5);
-      publisher_ = this->create_publisher<std_msgs::msg::String>("timestep", 10);
-      timer_ = this->create_wall_timer(
-      5ms, std::bind(&Nusim::timer_callback, this));
+      declare_parameter("rate", 200);
+      int rate = get_parameter("rate").get_parameter_value().get<int>();
+      publisher_ = create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
+      timer_ = create_wall_timer(std::chrono::milliseconds(1000/rate),
+      std::bind(&Nusim::timer_callback, this));
     }
 
   private:
     void timer_callback()
     {
-      auto message = std_msgs::msg::String();
-      message.data = "Hello, world! " + std::to_string(count_++);
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+      auto message = std_msgs::msg::UInt64();
+      message.data = timestep_++;
+      RCLCPP_INFO_STREAM(get_logger(), "Publishing:  " << message.data);
       publisher_->publish(message);
     }
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    size_t count_;
+    rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr publisher_;
+    size_t timestep_;
 };
 
 int main(int argc, char * argv[])
