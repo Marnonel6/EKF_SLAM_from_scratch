25650886b66e1804868e47128ecfa4079d607814
Marno (Mathinus) Nel <marthinusnel2023@u.northwestern.edu>
2023-01-20 14:26:19 -0600

    Teleport and reset service works, but not orientation

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -10,6 +10,7 @@
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2_ros/transform_broadcaster.h"
+#include "nusim/srv/teleport.hpp"
 
 // #include "nusim/srv/Empty.srv"
 
@@ -24,18 +25,32 @@ class Nusim : public rclcpp::Node
     Nusim(): Node("Nusim"), timestep_(0)
     {
       // Parameters
-      declare_parameter("rate", 200);
+      declare_parameter("rate", 200); // Hz for timer_callback
+      // Initial pose
+      declare_parameter("x0", 0.0);
+      declare_parameter("y0", 0.0);
+      declare_parameter("theta0", 0.0);
+      // Current pose
+      declare_parameter("x", 0.0);
+      declare_parameter("y", 0.0);
+      declare_parameter("theta", 0.0);
       int rate = get_parameter("rate").get_parameter_value().get<int>();
-      declare_parameter("x0", 0);
-      declare_parameter("y0", 0);
-      declare_parameter("theta0", 0);
+      double x0 = get_parameter("x0").get_parameter_value().get<double>();
+      double y0 = get_parameter("y0").get_parameter_value().get<double>();
+      double theta0 = get_parameter("theta0").get_parameter_value().get<double>();
+      double x = get_parameter("x").get_parameter_value().get<double>();
+      double y = get_parameter("y").get_parameter_value().get<double>();
+      double theta = get_parameter("theta").get_parameter_value().get<double>();
 
       // Publisher
       publisher_ = create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
 
       // Reset service
-      server_ = create_service<std_srvs::srv::Empty>("~/reset",
+      reset_server_ = create_service<std_srvs::srv::Empty>("~/reset",
       std::bind(&Nusim::reset_callback, this, std::placeholders::_1, std::placeholders::_2));
+      // Teleport service
+      teleport_server_ = create_service<nusim::srv::Teleport>("~/teleport",
+      std::bind(&Nusim::teleport_callback, this, std::placeholders::_1, std::placeholders::_2));
 
       // Initialize the transform broadcaster
       tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
@@ -49,18 +64,34 @@ class Nusim : public rclcpp::Node
     // Variables
     size_t timestep_;
     std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+    float x, y, theta;
+    float x0 = 0;
+    float y0 = 0;
+    float theta0 = 0;
 
     // Create objects
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr publisher_;
-    rclcpp::Service<std_srvs::srv::Empty>::SharedPtr server_;
+    rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reset_server_;
+    rclcpp::Service<nusim::srv::Teleport>::SharedPtr teleport_server_;
 
     // Reset service callback
     void reset_callback(std_srvs::srv::Empty::Request::SharedPtr ,std_srvs::srv::Empty::Response::SharedPtr)
     {
-      timestep_ = 0; // Reset timesetp
+      timestep_ = 0; // Reset timestep
+      x = x0;
+      y = y0;
+      theta = theta0;
     }
-    
+
+    // teleport service callback
+    void teleport_callback(nusim::srv::Teleport::Request::SharedPtr request, nusim::srv::Teleport::Response::SharedPtr response)
+    {
+      x = request->x;
+      y = request->y;
+      theta = request->theta;
+    }
+
     void broadcast_red_turtle()
     {
       geometry_msgs::msg::TransformStamped t;
@@ -73,9 +104,9 @@ class Nusim : public rclcpp::Node
 
       // Turtle only exists in 2D, thus we get x and y translation
       // coordinates from the message and set the z coordinate to 0
-      t.transform.translation.x = 0.0;
-      t.transform.translation.y = 0.0;
-      t.transform.translation.z = 0.0;
+      t.transform.translation.x = x;
+      t.transform.translation.y = y;
+      t.transform.translation.z = theta;
 
       // For the same reason, turtle can only rotate around one axis
       // and this why we set rotation in x and y to 0 and obtain
