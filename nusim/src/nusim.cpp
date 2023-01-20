9da394056a56e8bab8453ffe23a50c46304f2ac7
Marno (Mathinus) Nel <marthinusnel2023@u.northwestern.edu>
2023-01-20 13:17:31 -0600

    Robot is in rviz and broadcaster works C3.2

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -7,6 +7,10 @@
 #include "std_msgs/msg/string.hpp"
 #include "std_msgs/msg/u_int64.hpp"
 #include "std_srvs/srv/empty.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2_ros/transform_broadcaster.h"
+
 // #include "nusim/srv/Empty.srv"
 
 using namespace std::chrono_literals;
@@ -22,6 +26,9 @@ class Nusim : public rclcpp::Node
       // Parameters
       declare_parameter("rate", 200);
       int rate = get_parameter("rate").get_parameter_value().get<int>();
+      declare_parameter("x0", 0);
+      declare_parameter("y0", 0);
+      declare_parameter("theta0", 0);
 
       // Publisher
       publisher_ = create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
@@ -30,31 +37,67 @@ class Nusim : public rclcpp::Node
       server_ = create_service<std_srvs::srv::Empty>("~/reset",
       std::bind(&Nusim::reset_callback, this, std::placeholders::_1, std::placeholders::_2));
 
+      // Initialize the transform broadcaster
+      tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+
       // Timer
       timer_ = create_wall_timer(std::chrono::milliseconds(1000/rate),
       std::bind(&Nusim::timer_callback, this));
     }
 
-
   private:
+    // Variables
+    size_t timestep_;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+
+    // Create objects
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr publisher_;
+    rclcpp::Service<std_srvs::srv::Empty>::SharedPtr server_;
+
+    // Reset service callback
+    void reset_callback(std_srvs::srv::Empty::Request::SharedPtr ,std_srvs::srv::Empty::Response::SharedPtr)
+    {
+      timestep_ = 0; // Reset timesetp
+    }
+    
+    void broadcast_red_turtle()
+    {
+      geometry_msgs::msg::TransformStamped t;
+
+      // Read message content and assign it to
+      // corresponding tf variables
+      t.header.stamp = this->get_clock()->now();
+      t.header.frame_id = "nusim/world";
+      t.child_frame_id = "red/base_footprint";
+
+      // Turtle only exists in 2D, thus we get x and y translation
+      // coordinates from the message and set the z coordinate to 0
+      t.transform.translation.x = 0.0;
+      t.transform.translation.y = 0.0;
+      t.transform.translation.z = 0.0;
+
+      // For the same reason, turtle can only rotate around one axis
+      // and this why we set rotation in x and y to 0 and obtain
+      // rotation in z axis from the message
+      tf2::Quaternion q;
+      q.setRPY(0, 0, 0); // Last one should be theta
+      t.transform.rotation.x = q.x();
+      t.transform.rotation.y = q.y();
+      t.transform.rotation.z = q.z();
+      t.transform.rotation.w = q.w();
+
+      // Send the transformation
+      tf_broadcaster_->sendTransform(t);
+    }
+
     void timer_callback()
     {
       auto message = std_msgs::msg::UInt64();
       message.data = timestep_++;
       RCLCPP_INFO_STREAM(get_logger(), "Publishing:  " << message.data);
       publisher_->publish(message);
-    }
-
-    size_t timestep_;
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr publisher_;
-    rclcpp::Service<std_srvs::srv::Empty>::SharedPtr server_;
-
-
-    // Reset service
-    void reset_callback(std_srvs::srv::Empty::Request::SharedPtr ,std_srvs::srv::Empty::Response::SharedPtr)
-    {
-        timestep_ = 0; // Reset timesetp
+      broadcast_red_turtle();
     }
 };
 
