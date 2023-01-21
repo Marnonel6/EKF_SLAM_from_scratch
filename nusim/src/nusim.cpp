d3d9180076feda4ba707cf4aabbe0be52f1f9fa8
Marno (Mathinus) Nel <marthinusnel2023@u.northwestern.edu>
2023-01-21 15:51:00 -0600

    Added doxygen comments

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -1,3 +1,34 @@
+/// \file
+/// \brief The nusim package is a simulation and visualization tool for the turtlebot3 robots.
+///        It uses rviz2 for visualization and provides a simulated environment. The package
+///        will be able to create stationary walls and obstacles and track the position of a
+///        robot. 
+///
+/// PARAMETERS:
+///     \param rate (int): Timer callback frequency [Hz]
+///     \param x0_ (float): Initial x coordinate of the robot [m]
+///     \param y0_ (float): Initial y coordinate of the robot [m]
+///     \param theta0_ (float): Initial theta angle of the robot [radians]
+///     \param obstacles.x_ (std::vector<double>): Vector of x coordinates for each obstacle [m]
+///     \param obstacles.y_ (std::vector<double>): Vector of y coordinates for each obstacle [m]
+///     \param obstacles.r_ (float): Radius of cylindrical obstacles [m]
+///     \param obstacles.h_ (float): Height of cylindrical obstacles [m]
+///
+/// PUBLISHES:
+///     \param ~/timestep (std_msgs::msg::UInt64): Current simulation timestep
+///     \param ~/obstacles (visualization_msgs::msg::MarkerArray): Marker obstacles that are
+///                                                                displayed in Rviz
+///
+/// SUBSCRIBES:
+///     None
+///
+/// SERVERS:
+///     \param ~/reset (std_srvs::srv::Empty): Resets simulation to initial state
+///     \param ~/teleport (nusim::srv::Teleport): Teleport robot to a specific pose
+///
+/// CLIENTS:
+///     None
+
 #include <chrono>
 #include <functional>
 #include <memory>
@@ -18,6 +49,28 @@ using namespace std::chrono_literals;
 
 class Nusim : public rclcpp::Node
 {
+  /// \brief This class publishes the current timestep of the simulation and obstacles that
+  ///        appear in Rviz as markers. The class has a timer_callback to continually update the
+  ///        simulation at each timestep. The reset service resets the simulation to the initial
+  ///        state thus restarting the simulation. A teleport service is available to teleport a
+  ///        turtlebot to any pose. A broadcaster broadcasts the robots TF frames to a topic for
+  ///        visualization in Rviz. The simulation operates in a loop, updating the state of the
+  ///        world, publishing messages that provides state information simulating a real robot,
+  ///        and processing service/subscriber callbacks for commands for the next time step. The
+  ///        loop runs at a fixed frequency until termination.
+  ///
+  ///  \param rate (int): Timer callback frequency [Hz]
+  ///  \param x0_ (float): Initial x coordinate of the robot [m]
+  ///  \param y0_ (float): Initial y coordinate of the robot [m]
+  ///  \param theta0_ (float): Initial theta angle of the robot [radians]
+  ///  \param x_ (float): Current x coordinate of the robot [m]
+  ///  \param y_ (float): Current y coordinate of the robot [m]
+  ///  \param theta_ (float): Current theta angle of the robot [radians]
+  ///  \param obstacles.x_ (std::vector<double>): Vector of x coordinates for each obstacle [m]
+  ///  \param obstacles.y_ (std::vector<double>): Vector of y coordinates for each obstacle [m]
+  ///  \param obstacles.r_ (float): Radius of cylindrical obstacles [m]
+  ///  \param obstacles.h_ (float): Height of cylindrical obstacles [m]
+
   public:
     Nusim(): Node("Nusim"), timestep_(0)
     {
@@ -50,21 +103,21 @@ class Nusim : public rclcpp::Node
       declare_parameter("obstacles.h", 0.0, obstacles_h_des);
       // Get params - Read params from yaml file that is passed in the launch file
       int rate = get_parameter("rate").get_parameter_value().get<int>();
-      x0 = get_parameter("x0").get_parameter_value().get<float>();
-      y0 = get_parameter("y0").get_parameter_value().get<float>();
-      theta0 = get_parameter("theta0").get_parameter_value().get<float>();
-      obstacles_x = get_parameter("obstacles.x").get_parameter_value().get<std::vector<double>>();
-      obstacles_y = get_parameter("obstacles.y").get_parameter_value().get<std::vector<double>>();
-      obstacles_r = get_parameter("obstacles.r").get_parameter_value().get<float>();
-      obstacles_h = get_parameter("obstacles.h").get_parameter_value().get<float>();
+      x0_ = get_parameter("x0").get_parameter_value().get<float>();
+      y0_ = get_parameter("y0").get_parameter_value().get<float>();
+      theta0_ = get_parameter("theta0").get_parameter_value().get<float>();
+      obstacles_x_ = get_parameter("obstacles.x").get_parameter_value().get<std::vector<double>>();
+      obstacles_y_ = get_parameter("obstacles.y").get_parameter_value().get<std::vector<double>>();
+      obstacles_r_ = get_parameter("obstacles.r").get_parameter_value().get<float>();
+      obstacles_h_ = get_parameter("obstacles.h").get_parameter_value().get<float>();
 
       // Set current robot pose equal to initial pose
-      x = x0;
-      y = y0;
-      theta = theta0;
+      x_ = x0_;
+      y_ = y0_;
+      theta_ = theta0_;
 
       // Create obstacles
-      obstacles_ = create_obstacles_array(obstacles_x, obstacles_y, obstacles_r, obstacles_h);
+      create_obstacles_array();
 
       // Publisher
       timestep_publisher_ = create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
@@ -89,14 +142,14 @@ class Nusim : public rclcpp::Node
     // Variables
     size_t timestep_;
     std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
-    float x, y, theta; // Theta in radians, x & y in meters.
-    float x0 = 0;
-    float y0 = 0;
-    float theta0 = 0;
-    float obstacles_r;
-    float obstacles_h;
-    std::vector<double> obstacles_x;
-    std::vector<double> obstacles_y;
+    float x_, y_, theta_; // Theta in radians, x & y in meters.
+    float x0_ = 0;
+    float y0_ = 0;
+    float theta0_ = 0;
+    float obstacles_r_;
+    float obstacles_h_;
+    std::vector<double> obstacles_x_;
+    std::vector<double> obstacles_y_;
     visualization_msgs::msg::MarkerArray obstacles_;
 
     // Create objects
@@ -106,108 +159,94 @@ class Nusim : public rclcpp::Node
     rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reset_server_;
     rclcpp::Service<nusim::srv::Teleport>::SharedPtr teleport_server_;
 
-    // Reset service callback
+    /// \brief Reset the simulation
     void reset_callback(std_srvs::srv::Empty::Request::SharedPtr ,std_srvs::srv::Empty::Response::SharedPtr)
     {
-      timestep_ = 0; // Reset timestep
-      x = x0;
-      y = y0;
-      theta = theta0;
+      timestep_ = 0;
+      x_ = x0_;
+      y_ = y0_;
+      theta_ = theta0_;
     }
 
-    // teleport service callback
+    /// \brief Teleport the robot to a specified pose
     void teleport_callback(nusim::srv::Teleport::Request::SharedPtr request, nusim::srv::Teleport::Response::SharedPtr)
     {
-      x = request->x;
-      y = request->y;
-      theta = request->theta;
+      x_ = request->x;
+      y_ = request->y;
+      theta_ = request->theta;
     }
 
+    /// \brief Broadcast the TF frames of the robot
     void broadcast_red_turtle()
     {
-      geometry_msgs::msg::TransformStamped t;
+      geometry_msgs::msg::TransformStamped t_;
 
-      // Read message content and assign it to
-      // corresponding tf variables
-      t.header.stamp = this->get_clock()->now();
-      t.header.frame_id = "nusim/world";
-      t.child_frame_id = "red/base_footprint";
+      t_.header.stamp = this->get_clock()->now();
+      t_.header.frame_id = "nusim/world";
+      t_.child_frame_id = "red/base_footprint";
+      t_.transform.translation.x = x_;
+      t_.transform.translation.y = y_;
+      t_.transform.translation.z = 0.0; // Turtle only exists in 2D
 
-      // Turtle only exists in 2D, thus we get x and y translation
-      // coordinates from the message and set the z coordinate to 0
-      t.transform.translation.x = x;
-      t.transform.translation.y = y;
-      t.transform.translation.z = 0.0;
-
-      // For the same reason, turtle can only rotate around one axis
-      // and this why we set rotation in x and y to 0 and obtain
-      // rotation in z axis from the message
-      tf2::Quaternion q;
-      q.setRPY(0, 0, theta);
-      t.transform.rotation.x = q.x();
-      t.transform.rotation.y = q.y();
-      t.transform.rotation.z = q.z();
-      t.transform.rotation.w = q.w();
+      tf2::Quaternion q_;
+      q_.setRPY(0, 0, theta_); // Rotation around z-axis
+      t_.transform.rotation.x = q_.x();
+      t_.transform.rotation.y = q_.y();
+      t_.transform.rotation.z = q_.z();
+      t_.transform.rotation.w = q_.w();
 
       // Send the transformation
-      tf_broadcaster_->sendTransform(t);
+      tf_broadcaster_->sendTransform(t_);
     }
 
-     visualization_msgs::msg::MarkerArray create_obstacles_array(std::vector<double> obstacles_x,
-                                                                 std::vector<double> obstacles_y,
-                                                                 float obstacles_r,
-                                                                 float obstacles_h)
+    /// \brief Create obstacles as a MarkerArray and publish them to a topic to display them in Rviz
+    void create_obstacles_array()
     {
-
-      if (obstacles_x.size()!=obstacles_y.size())
+      if (obstacles_x_.size()!=obstacles_y_.size())
       {
-        int err = true;
+        int err_ = true;
         RCLCPP_ERROR(this->get_logger(), "x and y coordinate lists are not the same lenght!");
-        throw err;
+        throw err_;
       }
 
-      // Create array for obstacles
-      visualization_msgs::msg::MarkerArray obstacles_array;
-
-      for (int i = 0; i<(int)obstacles_x.size(); i++)
+      for (int i = 0; i<(int)obstacles_x_.size(); i++)
       {
-        visualization_msgs::msg::Marker obstacle;
-        obstacle.header.frame_id = "nusim/world";
-        obstacle.header.stamp = this->get_clock()->now();
-        obstacle.id = i;
-        obstacle.type = visualization_msgs::msg::Marker::CYLINDER;
-        obstacle.action = visualization_msgs::msg::Marker::ADD;
-        obstacle.pose.position.x = obstacles_x[i];
-        obstacle.pose.position.y = obstacles_y[i];
-        obstacle.pose.position.z = obstacles_h/2.0;
-        obstacle.pose.orientation.x = 0.0;
-        obstacle.pose.orientation.y = 0.0;
-        obstacle.pose.orientation.z = 0.0;
-        obstacle.pose.orientation.w = 1.0;
-        obstacle.scale.x = obstacles_r*2.0;
-        obstacle.scale.y = obstacles_r*2.0;
-        obstacle.scale.z = obstacles_h;
-        obstacle.color.r = 0.3058f;
-        obstacle.color.g = 0.1647f;
-        obstacle.color.b = 0.5176f;
-        obstacle.color.a = 1.0;
-        obstacles_array.markers.push_back(obstacle);
+        visualization_msgs::msg::Marker obstacle_;
+        obstacle_.header.frame_id = "nusim/world";
+        obstacle_.header.stamp = this->get_clock()->now();
+        obstacle_.id = i;
+        obstacle_.type = visualization_msgs::msg::Marker::CYLINDER;
+        obstacle_.action = visualization_msgs::msg::Marker::ADD;
+        obstacle_.pose.position.x = obstacles_x_[i];
+        obstacle_.pose.position.y = obstacles_y_[i];
+        obstacle_.pose.position.z = obstacles_h_/2.0;
+        obstacle_.pose.orientation.x = 0.0;
+        obstacle_.pose.orientation.y = 0.0;
+        obstacle_.pose.orientation.z = 0.0;
+        obstacle_.pose.orientation.w = 1.0;
+        obstacle_.scale.x = obstacles_r_*2.0; // Diameter in x
+        obstacle_.scale.y = obstacles_r_*2.0; // Diameter in y
+        obstacle_.scale.z = obstacles_h_;     // Height
+        obstacle_.color.r = 0.3058f;
+        obstacle_.color.g = 0.1647f;
+        obstacle_.color.b = 0.5176f;
+        obstacle_.color.a = 1.0;
+        obstacles_.markers.push_back(obstacle_);
       }
-
-      return obstacles_array;
     }
 
+    /// \brief Main simulation timer loop
     void timer_callback()
     {
       auto message = std_msgs::msg::UInt64();
       message.data = timestep_++;
-      // LEAVE - RCLCPP_INFO_STREAM(get_logger(), "Publishing:  " << message.data);
       timestep_publisher_->publish(message);
       obstacles_publisher_->publish(obstacles_);
       broadcast_red_turtle();
     }
 };
 
+/// \brief Main function for node create, error handel and shutdown
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
@@ -215,7 +254,7 @@ int main(int argc, char * argv[])
   {
     rclcpp::spin(std::make_shared<Nusim>());
   }
-  catch(int err)
+  catch(int err_)
   {
     RCLCPP_ERROR(std::make_shared<Nusim>()->get_logger(), "x and y coordinate lists are not the same lenght!");
   }
