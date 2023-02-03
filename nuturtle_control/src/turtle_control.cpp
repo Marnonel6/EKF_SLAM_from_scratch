ec256af57cec43f6ba9f25828a3462bad6a96de7
Marno (Mathinus) Nel <marthinusnel2023@u.northwestern.edu>
2023-02-03 15:05:46 -0600

    Added cmd_vel subscriber

--- nuturtle_control/src/turtle_control.cpp
+++ nuturtle_control/src/turtle_control.cpp
@@ -8,6 +8,7 @@
 #include "geometry_msgs/msg/twist.hpp"
 
 using namespace std::chrono_literals;
+using std::placeholders::_1;
 
 class turtle_control : public rclcpp::Node
 {
@@ -15,8 +16,14 @@ class turtle_control : public rclcpp::Node
     turtle_control()
     : Node("turtle_control")
     {
-        // Publisher
-        cmd_vel_publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+        // Publishers
+        // cmd_vel_publisher_ = create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+
+        // Subscribers
+        cmd_vel_subscriber_ = create_subscription<geometry_msgs::msg::Twist>(
+                        "cmd_vel", 10, std::bind(&turtle_control::cmd_vel_callback, this, _1));
+
+        // Timer
         timer_ = create_wall_timer(500ms, std::bind(&turtle_control::timer_callback, this));
     }
 
@@ -25,15 +32,23 @@ class turtle_control : public rclcpp::Node
 
     // Create objects
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_publisher_;
+    // rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_publisher_;
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_subscriber_;
+
+    /// \brief
+    /// \param msg
+    void cmd_vel_callback(const geometry_msgs::msg::Twist & msg) const
+    {
+      RCLCPP_INFO(get_logger(), "I heard data");
+    }
 
     /// \brief Main simulation timer loop
     void timer_callback()
     {
-        auto message = geometry_msgs::msg::Twist();
-        message.linear.x = 1;
-        RCLCPP_INFO(get_logger(), "Publishing: '%f'", message.linear.x);
-        cmd_vel_publisher_->publish(message);
+        // auto message = geometry_msgs::msg::Twist();
+        // message.linear.x = 1;
+        // RCLCPP_INFO(get_logger(), "Publishing: '%f'", message.linear.x);
+        // cmd_vel_publisher_->publish(message);
     }
 };
 
