9ad32cc12247990b5c725c8bf004b0f0f0309990
Marno (Mathinus) Nel <marthinusnel2023@u.northwestern.edu>
2023-02-04 22:37:11 -0600

    Done with circle node

--- nuturtle_control/README.md
+++ nuturtle_control/README.md
@@ -1,1 +1,4 @@
 ros2 launch nuturtle_control start_robot.launch cmd_src:=teleop body_id:=base_footprint left_wheel_joint:=left_wheel right_wheel_joint:=right_wheel
+ros2 service call /control nuturtle_control/srv/Control "{velocity: 1.0, radius: 1.0}"
+ros2 service call /reverse std_srvs/srv/Empty "{}"
+ros2 service call /stop std_srvs/srv/Empty "{}"
--- nuturtle_control/src/circle.cpp
+++ nuturtle_control/src/circle.cpp
@@ -5,6 +5,9 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "nuturtle_control/srv/initial_config.hpp"
+#include "nuturtle_control/srv/control.hpp"
+#include "std_srvs/srv/empty.hpp"
 
 using namespace std::chrono_literals;
 
@@ -14,26 +17,80 @@ class circle : public rclcpp::Node
     circle()
     : Node("circle")
     {
+        // Parameter descirption
+        auto frequency_des = rcl_interfaces::msg::ParameterDescriptor{};
+        frequency_des.description = "Timer callback frequency [Hz]";
+        // Declare default parameters values
+        declare_parameter("frequency", 100, frequency_des);   // Hz for timer_callback
+        // Get params - Read params from yaml file that is passed in the launch file
+        int frequency = get_parameter("frequency").get_parameter_value().get<int>();
+
         // Publishers
-        cmd_vel_subscriber_ = create_publisher<geometry_msgs::msg::Twist>(
+        cmd_vel_publisher_ = create_publisher<geometry_msgs::msg::Twist>(
                                "cmd_vel", 10);
 
+        // Control server
+        control_server_ = create_service<nuturtle_control::srv::Control>("control",
+        std::bind(&circle::control_callback, this, std::placeholders::_1, std::placeholders::_2));
+        // Reverse server
+        reverse_server_ = create_service<std_srvs::srv::Empty>("reverse",
+        std::bind(&circle::reverse_callback, this, std::placeholders::_1, std::placeholders::_2));
+        // Stop server
+        stop_server_ = create_service<std_srvs::srv::Empty>("stop",
+        std::bind(&circle::stop_callback, this, std::placeholders::_1, std::placeholders::_2));
+
         // Timer
-        timer_ = create_wall_timer(10ms, std::bind(&circle::timer_callback, this));
+        timer_ = create_wall_timer(std::chrono::milliseconds(1000 / frequency),
+                                   std::bind(&circle::timer_callback, this));
     }
 
   private:
     // Variables
+    bool Flag_stop_ = false;
     geometry_msgs::msg::Twist body_twist_;
 
     // Create objects
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_subscriber_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_publisher_;
+    rclcpp::Service<nuturtle_control::srv::Control>::SharedPtr control_server_;
+    rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reverse_server_;
+    rclcpp::Service<std_srvs::srv::Empty>::SharedPtr stop_server_;
     rclcpp::TimerBase::SharedPtr timer_;
+
+    /// \brief control service callback
+    void control_callback(nuturtle_control::srv::Control::Request::SharedPtr request,
+                          nuturtle_control::srv::Control::Response::SharedPtr)
+    {
+        body_twist_.linear.x = request->radius*request->velocity;
+        body_twist_.angular.z = request->velocity;
+        Flag_stop_ = false;
+    }
+
+    /// \brief reverse service callback
+    void reverse_callback(std_srvs::srv::Empty::Request::SharedPtr,
+                          std_srvs::srv::Empty::Response::SharedPtr)
+    {
+        body_twist_.linear.x = -body_twist_.linear.x;
+        body_twist_.angular.z = -body_twist_.angular.z;
+        Flag_stop_ = false;
+    }
+
+    /// \brief stop service callback
+    void stop_callback(std_srvs::srv::Empty::Request::SharedPtr,
+                          std_srvs::srv::Empty::Response::SharedPtr)
+    {
+        body_twist_.linear.x = 0.0;
+        body_twist_.angular.z = 0.0;
+        cmd_vel_publisher_->publish(body_twist_);
+        Flag_stop_ = true;
+    }
 
     /// \brief Main timer loop
     void timer_callback()
     {
-        cmd_vel_subscriber_->publish(body_twist_);
+        if (Flag_stop_ == false)
+        {
+            cmd_vel_publisher_->publish(body_twist_);
+        }
     }
 };
 
