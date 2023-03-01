57e25ff972697d55903c48b5608296017c396a1f
Marno (Mathinus) Nel <marthinusnel2023@u.northwestern.edu>
2023-03-01 16:14:16 -0600

    added obsytacled for slam, but red turtle moves fast

--- nuslam/src/slam.cpp
+++ nuslam/src/slam.cpp
@@ -9,11 +9,15 @@
 ///     \param wheel_right (std::string): The name of the right wheel joint
 ///     \param wheelradius (double): The radius of the wheels [m]
 ///     \param track_width (double): The distance between the wheels [m]
+///     \param obstacles.r (double): Radius of cylindrical obstacles [m]
+///     \param obstacles.h (double): Height of cylindrical obstacles [m]
 ///
 /// PUBLISHES:
 ///     \param /odom (nav_msgs::msg::Odometry): Odometry publisher
 ///     \param /green/path (nav_msgs::msg::Path): Create the green turtle's nav_msgs/Path for rviz
 ///                                              visualization
+///     \param ~/obstacles (visualization_msgs::msg::MarkerArray): SLAM estimate marker obstacles that are
+///                                                                displayed in Rviz
 ///
 /// SUBSCRIBES:
 ///     \param /joint_states (sensor_msgs::msg::JointState): Subscribes joint states for green robot
@@ -46,6 +50,7 @@
 #include "nuturtle_control/srv/initial_config.hpp"
 #include "nav_msgs/msg/path.hpp"
 #include "visualization_msgs/msg/marker_array.hpp"
+#include "visualization_msgs/msg/marker.hpp"
 
 using namespace std::chrono_literals;
 
@@ -60,6 +65,8 @@ using namespace std::chrono_literals;
 ///  \param wheel_right_ (std::string): The name of the right wheel joint
 ///  \param wheelradius_ (double): The radius of the wheels [m]
 ///  \param track_width_ (double): The distance between the wheels [m]
+///  \param obstacles_r_ (double): Radius of cylindrical obstacles [m]
+///  \param obstacles_h_ (double): Height of cylindrical obstacles [m]
 
 class slam : public rclcpp::Node
 {
@@ -74,12 +81,16 @@ public:
     auto wheel_right_des = rcl_interfaces::msg::ParameterDescriptor{};
     auto wheelradius_des = rcl_interfaces::msg::ParameterDescriptor{};
     auto track_width_des = rcl_interfaces::msg::ParameterDescriptor{};
+    auto obstacles_r_des = rcl_interfaces::msg::ParameterDescriptor{};
+    auto obstacles_h_des = rcl_interfaces::msg::ParameterDescriptor{};
     body_id_des.description = "The name of the body frame of the robot";
     odom_id_des.description = "The name of the odometry frame";
     wheel_left_des.description = "The name of the left wheel joint";
     wheel_right_des.description = "The name of the right wheel joint";
     wheelradius_des.description = "The radius of the wheels [m]";
     track_width_des.description = "The distance between the wheels [m]";
+    obstacles_r_des.description = "Radius of cylindrical obstacles [m]";
+    obstacles_h_des.description = "Height of cylindrical obstacles [m]";
 
     // Declare default parameters values
     declare_parameter("body_id", "green/base_footprint", body_id_des);
@@ -88,6 +99,9 @@ public:
     declare_parameter("wheel_right", "green/wheel_right_link", wheel_right_des);
     declare_parameter("wheelradius", -1.0, wheelradius_des);
     declare_parameter("track_width", -1.0, track_width_des);
+    declare_parameter("obstacles.r", 0.0, obstacles_r_des);
+    declare_parameter("obstacles.h", 0.0, obstacles_h_des);
+
     // Get params - Read params from yaml file that is passed in the launch file
     body_id_ = get_parameter("body_id").get_parameter_value().get<std::string>();
     odom_id_ = get_parameter("odom_id").get_parameter_value().get<std::string>();
@@ -95,6 +109,8 @@ public:
     wheel_right_ = get_parameter("wheel_right").get_parameter_value().get<std::string>();
     wheelradius_ = get_parameter("wheelradius").get_parameter_value().get<double>();
     track_width_ = get_parameter("track_width").get_parameter_value().get<double>();
+    obstacles_r_ = get_parameter("obstacles.r").get_parameter_value().get<double>();
+    obstacles_h_ = get_parameter("obstacles.h").get_parameter_value().get<double>();
 
     // Ensures all values are passed via the launch file
     check_frame_params();
@@ -111,6 +127,8 @@ public:
     odom_publisher_ = create_publisher<nav_msgs::msg::Odometry>(
       "green/odom", 10);
     green_turtle_publisher_ = create_publisher<nav_msgs::msg::Path>("green/path", 10);
+    obstacles_publisher_ =
+      create_publisher<visualization_msgs::msg::MarkerArray>("~/obstacles", 10);
 
 
     // Subscribers
@@ -143,6 +161,8 @@ private:
   std::string wheel_right_;
   double wheelradius_;
   double track_width_;
+  double obstacles_r_;    // Size of obstacles
+  double obstacles_h_;
   int step_ = 0;
   turtlelib::Wheel new_wheel_pos_;
   turtlelib::Wheel prev_wheel_pos_{0.0, 0.0};
@@ -156,18 +176,17 @@ private:
   sensor_msgs::msg::JointState joint_states_;
   geometry_msgs::msg::PoseStamped green_pose_stamped_;
   nav_msgs::msg::Path green_path_;
-
-
-  // TODO
   turtlelib::Robot_configuration green_turtle{};
   turtlelib::Transform2D Tmap_RobotGreen{};
   turtlelib::Transform2D TodomGreen_RobotGreen{};
   turtlelib::Transform2D Tmap_odomGreen{};
   tf2::Quaternion q2_;
+  visualization_msgs::msg::MarkerArray obstacles_;
 
   // Create objects
   rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_publisher_;
   rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr green_turtle_publisher_;
+  rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr obstacles_publisher_;
   rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_states_subscriber_;
   rclcpp::Subscription<visualization_msgs::msg::MarkerArray>::SharedPtr fake_sensor_subscriber_;
   rclcpp::Service<nuturtle_control::srv::InitialConfig>::SharedPtr initial_pose_server_;
@@ -227,9 +246,6 @@ private:
             // RCLCPP_ERROR_STREAM(get_logger(), "\n zj_hat \n" << EKFSlam_.zj_hat);
         }
     }
-
-    // RCLCPP_ERROR_STREAM(get_logger(), "\n covariance = \n" << EKFSlam_.covariance);
-    // throw 1;
   }
 
   /// \brief Ensures all values are passed via the launch file
@@ -278,42 +294,62 @@ private:
   /// \brief Broadcasts green robots position
   void transform_broadcast_map_odom()
   {
-    // turtlelib::Robot_configuration green_turtle = EKFSlam_.EKFSlam_config();
-    // // turtlelib::Robot_configuration green_turtle = EKFSlam_.EKFSlam_config_predicted();
-
-    // turtlelib::Transform2D Tmap_RobotGreen{{green_turtle.x, green_turtle.y},green_turtle.theta};
-    // turtlelib::Transform2D TodomGreen_RobotGreen{{turtle_.configuration().x,turtle_.configuration().y},turtle_.configuration().theta};
-    // turtlelib::Transform2D Tmap_odomGreen{};
-
     green_turtle = EKFSlam_.EKFSlam_config();
-    // turtlelib::Robot_configuration green_turtle = EKFSlam_.EKFSlam_config_predicted();
-
     Tmap_RobotGreen = {{green_turtle.x, green_turtle.y},green_turtle.theta};
     TodomGreen_RobotGreen = {{turtle_.configuration().x,turtle_.configuration().y},turtle_.configuration().theta};
-    // Tmap_odomGreen{};
-
     Tmap_odomGreen = Tmap_RobotGreen*TodomGreen_RobotGreen.inv();
 
-    // RCLCPP_ERROR_STREAM(get_logger(), "green_turtle -> x: " << green_turtle.x << " y: " << green_turtle.y  << " theta: " << green_turtle.theta);
-    
-    // RCLCPP_ERROR_STREAM(get_logger(), "Tmap_odomGreen -> rot: " << Tmap_odomGreen.rotation()<< " trans: "<<Tmap_odomGreen.translation().x<<" "<<Tmap_odomGreen.translation().y);
     // Broadcast TF frames
     t2_.header.stamp = get_clock()->now();
     t2_.header.frame_id = "map";
     t2_.child_frame_id = odom_id_;
     t2_.transform.translation.x = Tmap_odomGreen.translation().x;
     t2_.transform.translation.y = Tmap_odomGreen.translation().y;
-    // t2_.transform.translation.x = -(green_turtle.x - turtle_.configuration().x);
-    // t2_.transform.translation.y = -(green_turtle.y - turtle_.configuration().y);
     t2_.transform.translation.z = 0.0;     // Turtle only exists in 2D
-    // tf2::Quaternion q2_;
     q2_.setRPY(0, 0, Tmap_odomGreen.rotation());       // Rotation around z-axis
-    // // q_.setRPY(0, 0, turtlelib::normalize_angle(-(green_turtle.theta - turtle_.configuration().theta)));       // Rotation around z-axis
     t2_.transform.rotation.x = q2_.x();
     t2_.transform.rotation.y = q2_.y();
     t2_.transform.rotation.z = q2_.z();
     t2_.transform.rotation.w = q2_.w();
     tf_broadcaster_2_->sendTransform(t2_);
+
+    // SLAM estimate of landmarks
+    create_obstacles_array();
+  }
+
+  /// \brief Create obstacles as a MarkerArray and publish them to a topic to display them in Rviz
+  void create_obstacles_array()
+  {
+
+    arma::colvec zai = EKFSlam_.EKFSlam_zai();
+    RCLCPP_ERROR_STREAM(get_logger(), "\n zai \n" << zai);
+
+
+    // for (auto i = 0; i < obstacles_x_.size(); i++) {
+    //   visualization_msgs::msg::Marker obstacle_;
+    //   obstacle_.header.frame_id = "nusim/world";
+    //   obstacle_.header.stamp = get_clock()->now();
+    //   obstacle_.id = i;
+    //   obstacle_.type = visualization_msgs::msg::Marker::CYLINDER;
+    //   obstacle_.action = visualization_msgs::msg::Marker::ADD;
+    //   obstacle_.pose.position.x = obstacles_x_.at(i);
+    //   obstacle_.pose.position.y = obstacles_y_.at(i);
+    //   obstacle_.pose.position.z = obstacles_h_ / 2.0;
+    //   obstacle_.pose.orientation.x = 0.0;
+    //   obstacle_.pose.orientation.y = 0.0;
+    //   obstacle_.pose.orientation.z = 0.0;
+    //   obstacle_.pose.orientation.w = 1.0;
+    //   obstacle_.scale.x = obstacles_r_ * 2.0;   // Diameter in x
+    //   obstacle_.scale.y = obstacles_r_ * 2.0;   // Diameter in y
+    //   obstacle_.scale.z = obstacles_h_;         // Height
+    //   obstacle_.color.r = 0.0f;
+    //   obstacle_.color.g = 1.0f;
+    //   obstacle_.color.b = 0.0f;
+    //   obstacle_.color.a = 1.0;
+    //   obstacles_.markers.push_back(obstacle_);
+    // }
+
+    // obstacles_publisher_->publish(obstacles_);
   }
 
   /// \brief Create the green turtle's nav_msgs/Path
