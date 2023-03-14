d0c52ba8e5c06a377d1260c54062ac5a210c9da8
Marno (Mathinus) Nel <marthinusnel2023@u.northwestern.edu>
2023-03-14 17:31:54 -0500

    Coded circle fitting

---
+++ turtlelib/include/turtlelib/circle_fitting.hpp
@@ -0,0 +1,40 @@
+#ifndef CIRCLE_FITTING_INCLUDE_GUARD_HPP
+#define CIRCLE_FITTING_INCLUDE_GUARD_HPP
+/// \file
+/// \brief Circle fitting algorithm
+// https://nu-msr.github.io/navigation_site/lectures/circle_fit.html
+// https://projecteuclid.org/journals/electronic-journal-of-statistics/volume-3/issue-none/Error-analysis-for-circle-fitting-algorithms/10.1214/09-EJS419.full
+
+#include <iosfwd>
+#include <cmath>
+#include "turtlelib/rigid2d.hpp"
+#include "turtlelib/diff_drive.hpp"
+#include <armadillo>
+#include <unordered_set>
+
+namespace turtlelib
+{
+
+    /// \brief Circle size and location
+    struct Circle
+    {
+        /// \brief the x coordinate of the circle's centroid
+        double a = 0.0;
+
+        /// \brief the y coordinate of the circle's centroid
+        double b = 0.0;
+
+        /// \brief the radius of the circle
+        double R = 0.0;
+    };
+
+    // void centroid_coordinates
+
+
+    /// \brief circle fitting algorithm
+    /// \param cluster (std::vector<turtlelib::Vector2D>) pass one cluster in to fit a circle to
+    Circle circle_fitting(std::vector<turtlelib::Vector2D> cluster);
+
+}
+
+#endif
---
+++ turtlelib/src/circle_fitting.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <armadillo>
+#include "turtlelib/circle_fitting.hpp"
+
+namespace turtlelib
+{
+
+    Circle circle_fitting(std::vector<turtlelib::Vector2D> cluster)
+    {
+        /*
+            Step 1
+        */
+        auto centroid_x_hat = 0.0;
+        auto centroid_y_hat = 0.0;
+        auto n_num = 0.0; // Total number of elements
+
+        for (size_t i = 0; i < cluster.size(); i++)
+        {
+            centroid_x_hat += cluster.at(i).x;
+            centroid_y_hat += cluster.at(i).y;
+            n_num+=1.0;
+        }
+
+        centroid_x_hat/=n_num; // eq...(1)
+        centroid_y_hat/=n_num; // eq...(2)
+
+        /*
+            Step 2: x_hat -> x & y_hat -> y
+        */
+        auto x_i = 0.0;
+        auto y_i = 0.0;
+        auto z_i = 0.0;
+        /*
+            Step 4
+        */
+        auto z_mean = 0.0;
+        auto n = static_cast<double>(cluster.size());
+        /*
+            Step 5: Data matrix
+        */
+        arma::mat Z{cluster.size(), 4, arma::fill::zeros};
+
+        for (size_t i = 0; i < cluster.size(); i++)
+        {
+            x_i = cluster.at(i).x - centroid_x_hat;          // eq...(3)
+            y_i = cluster.at(i).y - centroid_y_hat;          // eq...(4)
+            z_i = x_i*x_i + y_i*y_i;                         // Step 3
+
+            Z.insert_rows(i,arma::rowvec{z_i, x_i, y_i, 1}); // eq...(6)
+            z_mean += z_i;
+        }
+
+        /*
+            Step 4
+        */
+        z_mean = z_mean/n;
+
+        /*
+            Step 6: Moment matrix
+        */
+        arma::mat M = (1/n)*trans(Z)*Z;
+
+        /*
+            Step 7: Constraint matrix
+        */
+        arma::mat H(4, 4, arma::fill::zeros);
+        H(0,0) = 8.0*z_mean;    // eq...(7)
+        H(3,0) = 2.0;
+        H(0,3) = 2.0;
+        H(1,1) = 1.0;
+        H(2,2) = 1.0;
+
+        /*
+            Step 8: Constraint matrix inverse
+        */
+        arma::mat H_inv(4, 4, arma::fill::zeros);
+        H(3,3) = -2.0*z_mean;    // eq...(8)
+        H(3,0) = 0.5;
+        H(0,3) = 0.5;
+        H(1,1) = 1.0;
+        H(2,2) = 1.0;
+
+        /*
+            Step 9: Singular Value Decomposition of Z
+        */
+        arma::mat U{};
+        arma::vec s{};
+        arma::mat V{};
+        arma::svd(U, s, V, Z);    // eq...(9)
+
+        arma::colvec A{4, arma::fill::zeros};
+        if (s(3) < 10e-12)
+        {
+            /*
+                Step 10: sigma4 < 10^-12
+            */
+            // A(0) = V(0,3);
+            // A(1) = V(1,3);
+            // A(2) = V(2,3);
+            // A(3) = V(3,3);
+            A = V.col(3);
+        }
+        else
+        {
+            /*
+                Step 11: sigma4 > 10^-12
+            */
+            arma::mat Y = V*arma::diagmat(s)*trans(V);    // eq...(10)
+            arma::mat Q = Y*H_inv*Y;
+            // eig vec & val Q
+            arma::cx_vec eigval{};
+            arma::cx_mat eigvec{};
+            arma::eig_gen(eigval, eigvec, Q);
+            // Get real
+            arma::vec real_eigval = arma::real(eigval);
+            arma::mat real_eigvec = arma::real(eigvec);
+            // Set A* = smallest eig value's vec
+            auto min = 1000000.0;
+            size_t min_index = 0;
+            for (size_t i = 0; i < real_eigval.size(); i++)
+            {
+                if (real_eigval(0) < min) // Find the smallest eigval
+                {
+                    min = real_eigval(0);
+                    min_index = i;
+                }
+            }
+            // Extract eigvec that corresponds with the eigen value
+            arma::vec A_star = real_eigvec.col(min_index);
+            A = Y.i()*A_star;
+        }
+
+        /*
+            Step 12: Equation for circle eq...(11)
+        */
+        const auto a = -A(1)/(2.0*A(0));    // eq...(12)
+        const auto b = -A(2)/(2.0*A(0));    // eq...(13)
+        const auto R = std::sqrt((A(1)*A(1) + A(2)*A(2) - 4.0*A(0)*A(3)) / (4.0*A(0)*A(0))); // eq...(14)
+
+    return {a, b, R};
+    }
+
+}
